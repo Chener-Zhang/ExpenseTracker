5726968fab904e25d349b574e19e679c4c14cb7c
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-01-07 16:40:30 -0800

    successfully connect with the expense

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,21 +1,28 @@
 import React, { createContext, useReducer, useState } from "react";
 
+const initialState = {
+  budget: 5000,
+  expenses: []
+};
+
 const expenseReducer = (state, action) => {
   switch (action.type) {
+    case "ADD":
+      return { ...state, expenses: [...state.expenses, action.payload] };
+    case "REMOVE":
+      break;
+
     default:
       return state;
   }
 };
-const initialState = {
-  budget: 0,
-  expenses: []
-};
+
 export const ListContext = createContext();
 export const ListProvider = (props) => {
   const [state, dispatch] = useReducer(expenseReducer, initialState);
   return (
     <ListContext.Provider
-      value={{ budget: state.budget, expense: state.expense, dispatch }}
+      value={{ budget: state.budget, expenses: state.expenses, dispatch }}
     >
       {props.children}
     </ListContext.Provider>
--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -2,13 +2,23 @@ import React, { useContext, useState } from "react";
 import { ListContext } from "../AppContext";
 
 const AddExpense = () => {
-  const { budget } = useContext(ListContext);
   const [name, setName] = useState("");
   const [cost, setCost] = useState("");
+  const { dispatch, expenses } = useContext(ListContext);
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
-    console.log(budget);
+
+    const newExpense = {
+      name: name,
+      cost: parseInt(cost)
+    };
+
+    dispatch({
+      type: "ADD",
+      payload: newExpense
+    });
+    console.log(expenses);
   };
 
   return (
@@ -21,6 +31,7 @@ const AddExpense = () => {
         placeholder="Enter Name"
         required="required"
         type="text"
+        onChange={(event) => setName(event.target.value)}
       />
 
       <label htmlFor="amout">Amout</label>
@@ -30,6 +41,7 @@ const AddExpense = () => {
         placeholder="Enter Amout"
         required="required"
         type="text"
+        onChange={(event) => setCost(event.target.value)}
       />
       <button className="btn btn-primary mt-3">Submit</button>
     </form>
